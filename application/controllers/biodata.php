0010f77d41b64396f7e6cfc9f1f5ba5ea1d0aeb2
Muzakki02 <muzakkisyam65@gmail.com>
2022-03-26 13:37:08 +0700

    biodata

---
+++ application/controllers/biodata.php
@@ -0,0 +1,11 @@
+<?php
+class biodata extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Hallo, Perkenalkan</h1>";
+        echo"Nama Saya ACHMAD MUZAKKI SYAM
+            Saya Tinggal di RAWAMANGUN, JAKARTA TIMUR
+            Hobi saya berolahraga"; 
+    }
+}
